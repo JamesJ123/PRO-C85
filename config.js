bca780b2d1ae9f0a136d2579891bca3839eae2d5
JamesJ123 <85694114+JamesJ123@users.noreply.github.com>
2022-02-16 19:26:28 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCQypRpyb7ic_t-l4vk7iR-nvNdE22-Nz8",
+    authDomain: "storytellingapp-d3b64.firebaseapp.com",
+    projectId: "storytellingapp-d3b64",
+    storageBucket: "storytellingapp-d3b64.appspot.com",
+    messagingSenderId: "170235195459",
+    appId: "1:170235195459:web:fffdfd00f4767f15c664f6",
+    measurementId: "G-0XLVZQL7YM"
+  };
